e02db95be23d10df6c45bb777f79a6ae34a47603
Mrudulau30 <mrudulau30@gmail.com>
2021-07-05 19:57:13 +0530

    minor changes

--- THA-16/day16/src/index.js
+++ THA-16/day16/src/index.js
@@ -3,8 +3,11 @@ import  ReactDOM  from 'react-dom';
 import Card from './Components/card';
 
 
+  ReactDOM.render(
+    <Card />,
+      document.getElementById('root')
+  );
 
-ReactDOM.render(
-  <Card />,
-    document.getElementById('root')
-)
+
+
+
