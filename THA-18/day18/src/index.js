d69c27a43b8e34b27f0f1f4d6afee00846a5dfca
Mrudulau30 <mrudulau30@gmail.com>
2021-07-08 20:25:23 +0530

    done

---
+++ THA-18/day18/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Chess from './App';
+import './style.css';
+
+ReactDOM.render(
+  <div class="container">
+    <Chess />
+    </div>,
+  document.getElementById('root')
+);
+
