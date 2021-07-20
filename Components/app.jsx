6443929a081e5149b37d8de6bdfd42d0a940bb67
Mrudulau30 <mrudulau30@gmail.com>
2021-07-20 16:50:55 +0530

    structure fixed

---
+++ Components/app.jsx
@@ -0,0 +1,17 @@
+import Card from './card'
+import Chess from './chessboard';
+import "./style.css"
+
+function App(){
+    return(
+        <div className="wrapper">
+    <Card />
+   <div class="chessb">
+    <Chess /></div>
+    </div>
+   
+
+    );
+}
+
+export default App;
