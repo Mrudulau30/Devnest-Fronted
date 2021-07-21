a55446dbe8dfd0bc03899f57e63f82f9fa209255
Mrudulau30 <mrudulau30@gmail.com>
2021-07-21 16:08:26 +0530

    router dom check

---
+++ THA-024/day24/src/App.jsx
@@ -0,0 +1,18 @@
+
+import React,{useState,Component} from 'react'
+
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About';
+import Profile from './components/Profile'
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+   <div className="App">
+     <Route path="/"></Route>
+   </div>
+  );
+}
+
+export default App;
---
+++ THA-024/day24/src/components/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Dashboard=()=>{
+    return (
+    <h1>Dashboard in</h1>
+    );
+}
+export default Dashboard;
