0871ef68d4eba3e16d6eb144bedcbee951c1d559
Mrudulau30 <mrudulau30@gmail.com>
2021-08-01 18:19:26 +0530

    done

---
+++ THA-027/day27/src/actions/action.js
@@ -0,0 +1,14 @@
+export const addTodo = payload => {
+	return {
+		type: 'ADD_TODO',
+		payload,
+	};
+};
+
+export const removeTodo = payload => {
+	return {
+		type: 'REMOVE_TODO',
+		payload,
+	};
+};
+
---
+++ THA-027/day27/src/reducers/todoReducer.js
@@ -0,0 +1,14 @@
+const todoReducer = (state = [], action) => {
+	switch (action.type) {
+		case 'ADD_TODO':
+			return [...state, action.payload];
+			break;
+		case 'REMOVE_TODO':
+			return state.filter((_, index) => index !== action.payload);
+			break;
+		default:
+			return state;
+	}
+};
+
+export default todoReducer;
